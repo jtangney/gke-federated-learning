823ea38376f9852b3ab4885f4b83a44506a5db02
jtangney <jeremy.tangney@gmail.com>
2021-10-01 11:55:34 +0100

    tidy up

--- silo-iac/services.tf
+++ silo-iac/services.tf
@@ -4,22 +4,23 @@ module "project-services" {
   project_id  = var.project_id
   disable_services_on_destroy = false
   activate_apis = [
-    "cloudbilling.googleapis.com",
-    "compute.googleapis.com",
-    "iam.googleapis.com",
-    "container.googleapis.com",
-    "cloudresourcemanager.googleapis.com",
     "anthos.googleapis.com",
     "anthosconfigmanagement.googleapis.com",
+    "cloudbilling.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
     "cloudtrace.googleapis.com",
-    "meshca.googleapis.com",
-    "meshtelemetry.googleapis.com",
-    "meshconfig.googleapis.com",
-    "iamcredentials.googleapis.com",
+    "compute.googleapis.com",
+    "container.googleapis.com",
+    "dns.googleapis.com",
     "gkeconnect.googleapis.com",
     "gkehub.googleapis.com",
-    "monitoring.googleapis.com",
+    "iam.googleapis.com",
+    "iamcredentials.googleapis.com",
     "logging.googleapis.com",
+    "meshca.googleapis.com",
+    "meshconfig.googleapis.com",
+    "meshtelemetry.googleapis.com",
+    "monitoring.googleapis.com",
     "stackdriver.googleapis.com"
   ]
 }
