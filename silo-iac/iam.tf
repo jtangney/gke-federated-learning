e3f73dd3b4a29c1967c9e35f6e3176b339c5dc95
jtangney <jeremy.tangney@gmail.com>
2021-10-01 11:55:26 +0100

    baseline IAM for FL node pool SA

--- silo-iac/iam.tf
+++ silo-iac/iam.tf
@@ -1,3 +1,4 @@
+# Service Account used by the nodes in FL node pool
 resource "google_service_account" "cluster_flpool_sa" {
   project      = var.project_id
   account_id   = local.fl_node_pool_sa_name
@@ -11,4 +12,25 @@ resource "google_storage_bucket_iam_binding" "container_eu_pull_binding" {
     # format("serviceAccount:%s", google_service_account.cluster_default_sa.email),
     format("serviceAccount:%s", google_service_account.cluster_flpool_sa.email)
   ]
+}
+
+module "project-iam-bindings" {
+  source   = "terraform-google-modules/iam/google//modules/projects_iam"
+  projects = [var.project_id]
+  mode     = "authoritative"
+
+  bindings = {
+    "roles/logging.logWriter" = [
+      format("serviceAccount:%s", google_service_account.cluster_flpool_sa.email)
+    ]
+    "roles/monitoring.metricWriter" = [
+      format("serviceAccount:%s", google_service_account.cluster_flpool_sa.email)
+    ]
+    "roles/monitoring.viewer" = [
+      format("serviceAccount:%s", google_service_account.cluster_flpool_sa.email)
+    ]
+    "roles/artifactregistry.reader" = [
+      format("serviceAccount:%s", google_service_account.cluster_flpool_sa.email)
+    ]
+  }
 }
