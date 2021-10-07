b2ff73c2689e946a93a9c11ecee97ca63e846ce4
jtangney <jeremy.tangney@gmail.com>
2021-10-07 11:37:14 +0100

    workload identity

--- silo-iac/iam.tf
+++ silo-iac/iam.tf
@@ -1,8 +1,15 @@
-# Service Account used by the nodes in FL node pool
+# Service Account used by the nodes in tenant node pool
 resource "google_service_account" "tenant_nodepool_sa" {
   project      = var.project_id
   account_id   = local.tenant_nodepool_sa_name
   display_name = "Service account for ${local.tenant_nodepool_sa_name} node pool in cluster ${var.cluster_name}"
+}
+
+# Service Account used by apps in the tenant namespace
+resource "google_service_account" "tenant_apps_sa" {
+  project      = var.project_id
+  account_id   = local.tenant_apps_sa_name
+  display_name = "Service account for ${var.tenant_name} apps in cluster ${var.cluster_name}"
 }
 
 # resource "google_storage_bucket_iam_binding" "container_eu_pull_binding" {
@@ -34,3 +41,25 @@ module "project-iam-bindings" {
     ]
   }
 }
+
+resource "google_service_account_iam_binding" "tenant" {
+  service_account_id = google_service_account.tenant_apps_sa.name
+  role               = "roles/iam.workloadIdentityUser"
+
+  members = [
+    format("serviceAccount:%s.svc.id.goog[%s]", var.project_id, local.tenant_apps_ksa_name)
+  ]
+}
+
+# module "service_account-iam-bindings" {
+#   source = "terraform-google-modules/iam/google//modules/service_accounts_iam"
+#   project          = var.project_id
+#   mode             = "authoritative"
+  
+#   service_accounts = [google_service_account.tenant_apps_sa]
+#   bindings = {
+#     "roles/iam.workloadIdentityUser" = [
+#       format("serviceAccount:%s.svc.id.goog[%s]", var.project_id, local.tenant_apps_ksa_name),
+#     ]
+#   }
+# }
