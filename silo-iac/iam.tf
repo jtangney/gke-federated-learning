ab2a893749ae7547b8849ae5612ed1f1bbe578fe
jtangney <jeremy.tangney@gmail.com>
2021-10-13 12:21:43 +0100

    tidy up

--- silo-iac/iam.tf
+++ silo-iac/iam.tf
@@ -12,15 +12,6 @@ resource "google_service_account" "tenant_apps_sa" {
   display_name = "Service account for ${var.tenant_name} apps in cluster ${var.cluster_name}"
 }
 
-# resource "google_storage_bucket_iam_binding" "container_eu_pull_binding" {
-#   bucket = format("eu.artifacts.%s.appspot.com", var.project_id)
-#   role = "roles/storage.objectViewer"
-#   members = [
-#     # format("serviceAccount:%s", google_service_account.cluster_default_sa.email),
-#     format("serviceAccount:%s", google_service_account.tenant_nodepool_sa.email)
-#   ]
-# }
-
 module "project-iam-bindings" {
   source   = "terraform-google-modules/iam/google//modules/projects_iam"
   projects = [var.project_id]
@@ -52,16 +43,3 @@ resource "google_service_account_iam_binding" "tenant" {
     format("serviceAccount:%s.svc.id.goog[%s]", var.project_id, "testing/ksa")
   ]
 }
-
-# module "service_account-iam-bindings" {
-#   source = "terraform-google-modules/iam/google//modules/service_accounts_iam"
-#   project          = var.project_id
-#   mode             = "authoritative"
-  
-#   service_accounts = [google_service_account.tenant_apps_sa]
-#   bindings = {
-#     "roles/iam.workloadIdentityUser" = [
-#       format("serviceAccount:%s.svc.id.goog[%s]", var.project_id, local.tenant_apps_ksa_name),
-#     ]
-#   }
-# }
